40fde32e1b797575bd7267dfb349568bb2462ccb
Nathan Ferdinando <nferdica@outlook.com>
2024-09-11 21:42:41 -0300

    feat: Created routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
+import {createBrowserRouter} from "react-router-dom"
+import { Home } from "./pages/home"
+import { Login } from "./pages/login"
+import { Register } from "./pages/register"
+import { CarDetail } from "./pages/car"
+import { Dashboard } from "./pages/dashboard"
+import { New } from "./pages/dashboard/new"
+import { Layout } from "./components/layout"
 
-function App() {
-  return(
-    <div>
-      <h1>App</h1>
-    </div>
-  )
-}
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />,
+      },
+      {
+        path: '/dashboard/new',
+        element: <New />
+      },
+      {
+        path: '/car/:id',
+        element: <CarDetail />
+      }
+    ]
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+])
 
-export default App;
+export {router};
