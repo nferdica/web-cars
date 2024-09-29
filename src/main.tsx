b05e98d0f440951326944bc9221ae4b17dc69c64
Nathan Ferdinando <nferdica@outlook.com>
2024-09-28 23:13:14 -0300

    feat: Created context routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {router} from './App.tsx'
 import { RouterProvider } from 'react-router-dom'
+import AuthProvider from './context/AuthContext'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <AuthProvider>
     <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
